8dd8315c7b12536cb9a5e9d8455d86571cc615be
Samuel Rocha <samuel.afrocha@gmail.com>
2021-06-17 12:00:36 -0300

    Project start :godmode:
    Starting the project:
    
    :point_right: html and css basically done. 
    :point_right: index.js just created, nothing solid done.

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector("#campo-nome"))
+
+var contatos = [ 
+  document.querySelector("#campo-nome"),
+  document.querySelector("#campo-idade"),
+  document.querySelector("#campo-curso"),
+];
+
+console.log(contatos);
