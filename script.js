c51fb887e203ab3195a9a49f171a595819a66b7b
Samuel Rocha <samuel.afrocha@gmail.com>
2021-06-24 00:19:25 -0300

    ADD function implemented :hurtrealbad:
    The ADD function is now implemented, there was a slight change in the HTML code now it uses the tag <table> for better control

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-console.log(document.querySelector("#campo-nome"))
+function add(){
+  var contato = [
+    nome = document.getElementById('campo-nome').value,
+    idade = document.getElementById('campo-idade').value,
+    curso = document.getElementById('campo-curso').value
+  ]
+  var table = document.getElementById('table');
+  var row = table.insertRow(0)
+  var cellNome = row.insertCell(0)
+  var cellIdade = row.insertCell(1)
+  var cellCurso = row.insertCell(2)
+  cellNome.innerHTML = nome;
+  cellIdade.innerHTML = idade;
+  cellCurso.innerHTML = curso;
 
-var contatos = [ 
-  document.querySelector("#campo-nome"),
-  document.querySelector("#campo-idade"),
-  document.querySelector("#campo-curso"),
-];
-
-console.log(contatos);
+  //Limpando o input
+  document.getElementById('campo-nome').value = ''
+  document.getElementById('campo-idade').value = ''
+  document.getElementById('campo-curso').value = ''
+}
