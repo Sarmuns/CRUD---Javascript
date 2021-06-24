d4d99290aa92db9761ca7b632f76afd39eb99dec
Samuel Rocha <samuel.afrocha@gmail.com>
2021-06-24 12:43:17 -0300

    :rage1: Added a counter in contacts :rage1:
    Added the "#" field in the contacts table, so now every contact added will have an ID number shown
    Also when adding a new member to the contacts table it now places the new member below the already existing one (as it should have been from the beginning)

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+var aux = 2;
+
 function add(){
   var contato = [
     nome = document.getElementById('campo-nome').value,
     idade = document.getElementById('campo-idade').value,
     curso = document.getElementById('campo-curso').value
   ]
+
   var table = document.getElementById('table');
-  var row = table.insertRow(0)
-  var cellNome = row.insertCell(0)
-  var cellIdade = row.insertCell(1)
-  var cellCurso = row.insertCell(2)
+  var row = table.insertRow()
+  var cellAux = row.insertCell(0)
+  var cellNome = row.insertCell(1)
+  var cellIdade = row.insertCell(2)
+  var cellCurso = row.insertCell(3)
+  cellAux.innerHTML = aux;
   cellNome.innerHTML = nome;
   cellIdade.innerHTML = idade;
   cellCurso.innerHTML = curso;
+  aux = aux + 1;
 
   //Limpando o input
   document.getElementById('campo-nome').value = ''
