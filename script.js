369d2cffcf69d7dcca277f64a270cb24396c72cf
Samuel Rocha <samuel.afrocha@gmail.com>
2021-06-25 18:21:46 -0300

    :finnadie: project done :finnadie:
    * Numbers in the # field now work properly
    * Field "idade" only accept numbers now

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // aux = 2, pois já tem um contato na lista, ocupando o #1
-var aux = 2;
 
 // função para pegar os valores do formulario e limpa-lo
 function get(){
@@ -8,19 +7,20 @@ function get(){
     idade : document.getElementById('campo-idade').value,
     curso : document.getElementById('campo-curso').value
   }
-  
+
   document.getElementById('campo-nome').value = ''
   document.getElementById('campo-idade').value = ''
   document.getElementById('campo-curso').value = ''
-  
+
   return contatos;
 }
 
 function add(){
-//
+
   contato = get();
 // cria uma row no formato correto
   var table = document.getElementById('table');
+  var lastRow = table.rows.length;
   var row = table.insertRow()
   var cellAux = row.insertCell(0)
   var cellNome = row.insertCell(1)
@@ -30,19 +30,26 @@ function add(){
   var cellDelete = row.insertCell(5)
 
 // setando valores coletados
-  cellAux.innerHTML = aux;
+  cellAux.innerHTML = lastRow;
   cellNome.innerHTML = contato.nome;
   cellIdade.innerHTML = contato.idade;
   cellCurso.innerHTML = contato.curso;
   cellEdit.innerHTML = '<button onclick="edit(this)">Edit</button>';
   cellDelete.innerHTML = '<button onclick="del(this)">Delete</button>';
-
-  aux += 1
 }
 
 // função delete
 function del(el){
   el.parentElement.parentElement.remove();
+  
+  var table = document.getElementById('table');
+  
+  var i = 1;
+
+  while (i < table.rows.length){
+  table.rows[i].cells[0].innerHTML = i;
+    i += 1;
+  }
 }
 
 function edit(el){
