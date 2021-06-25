2c2e939e5a90a77f5088e2f17640e58ca1d5fc0d
Samuel Rocha <samuel.afrocha@gmail.com>
2021-06-25 17:41:55 -0300

    :rage3: Delete and Edit function done :rage3:
    Basically done with the project, will just add some business for better user experience

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 // aux = 2, pois já tem um contato na lista, ocupando o #1
 var aux = 2;
 
-function add(){
-  var contato = [
-    nome = document.getElementById('campo-nome').value,
-    idade = document.getElementById('campo-idade').value,
-    curso = document.getElementById('campo-curso').value
-  ]
+// função para pegar os valores do formulario e limpa-lo
+function get(){
+  var contatos = {
+    nome : document.getElementById('campo-nome').value,
+    idade : document.getElementById('campo-idade').value,
+    curso : document.getElementById('campo-curso').value
+  }
+  
+  document.getElementById('campo-nome').value = ''
+  document.getElementById('campo-idade').value = ''
+  document.getElementById('campo-curso').value = ''
+  
+  return contatos;
+}
 
+function add(){
+//
+  contato = get();
+// cria uma row no formato correto
   var table = document.getElementById('table');
   var row = table.insertRow()
   var cellAux = row.insertCell(0)
   var cellNome = row.insertCell(1)
   var cellIdade = row.insertCell(2)
   var cellCurso = row.insertCell(3)
-  cellAux.innerHTML = aux;
-  cellNome.innerHTML = nome;
-  cellIdade.innerHTML = idade;
-  cellCurso.innerHTML = curso;
-  aux += 1
+  var cellEdit = row.insertCell(4)
+  var cellDelete = row.insertCell(5)
 
-  //Limpando o input
-  document.getElementById('campo-nome').value = ''
-  document.getElementById('campo-idade').value = ''
-  document.getElementById('campo-curso').value = ''
+// setando valores coletados
+  cellAux.innerHTML = aux;
+  cellNome.innerHTML = contato.nome;
+  cellIdade.innerHTML = contato.idade;
+  cellCurso.innerHTML = contato.curso;
+  cellEdit.innerHTML = '<button onclick="edit(this)">Edit</button>';
+  cellDelete.innerHTML = '<button onclick="del(this)">Delete</button>';
+
+  aux += 1
+}
+
+// função delete
+function del(el){
+  el.parentElement.parentElement.remove();
+}
+
+function edit(el){
+// pegando dados novos do form
+  contato = get();
+// setando dados novos
+  var cells = el.parentElement.parentElement.cells;
+  cells[1].textContent = contato.nome;
+  cells[2].textContent = contato.idade;
+  cells[3].textContent = contato.curso;
 }
